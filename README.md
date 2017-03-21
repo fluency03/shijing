b5c7cab4c42282771a9b9169ce7c2d090a22d1e0
fluency03 <fluency.03@gmail.com>
2017-03-21 23:26:51 +0100

    modules

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# shi | 诗经
+# 诗经 | shi
 
 [![Build Status](https://travis-ci.org/fluency03/shi.svg?branch=master)](https://travis-ci.org/fluency03/shi)
 
--- shi.js
+++ shi.js
@@ -20,22 +20,31 @@ var mood = function () {
   return m[Math.floor(Math.random()*m.length)];
 };
 
-
-//A remote node repl that you can telnet to!
-net.createServer(function (socket) {
-  var remote = repl.start("node::remote> ", socket);
-  //Adding "mood" and "bonus" to the remote REPL's context.
-  remote.context.mood = mood;
-  remote.context.bonus = "UNLOCKED";
-}).listen(5001);
-
-
-logger.info("Remote REPL started on port 5001.");
+function shi() {
+  //A remote node repl that you can telnet to!
+  net.createServer(function (socket) {
+    var remote = repl.start("诗经::远程> ", socket);
+    //Adding "mood" and "bonus" to the remote REPL's context.
+    remote.context.mood = mood;
+    remote.context.诗经 = "shi";
+    remote.context.bonus = "UNLOCKED";
+  }).listen(5001);
 
 
-//A "local" node repl with a custom prompt
-var local = repl.start("诗经> ");
+  logger.info("Remote REPL started on port 5001.");
 
 
-// Exposing the function "mood" to the local REPL's context.
-local.context.mood = mood;
+  //A "local" node repl with a custom prompt
+  var local = repl.start("诗经::本地> ");
+
+
+  // Exposing the function "mood" to the local REPL's context.
+  local.context.mood = mood;
+  local.context.诗经 = "shi";
+  local.context.shi = "诗经";
+  local.context.作者 = "刘畅";
+}
+
+shi();
+
+module.exports = shi;
